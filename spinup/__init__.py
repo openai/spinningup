08fe0e6c7a90968740e08385808832184688716f
Akshit Khurana <akhurana@housecanary.com>
2019-01-13 13:38:16 -0800

    Add dqn draft

--- spinup/__init__.py
+++ spinup/__init__.py
@@ -1,5 +1,6 @@
 # Algorithms
 from spinup.algos.ddpg.ddpg import ddpg
+from spinup.algos.dqn.dqn import dqn
 from spinup.algos.ppo.ppo import ppo
 from spinup.algos.sac.sac import sac
 from spinup.algos.td3.td3 import td3
---
+++ spinup/algos/dqn/core.py
@@ -0,0 +1,56 @@
+import numpy as np
+import tensorflow as tf
+from gym.spaces import Box, Discrete
+
+
+def combined_shape(length, shape=None):
+    if shape is None:
+        return (length,)
+    return (length, shape) if np.isscalar(shape) else (length, *shape)
+
+
+def placeholder(dim=None):
+    return tf.placeholder(dtype=tf.float32, shape=(None, dim) if dim else (None,))
+
+
+def placeholders(*args):
+    return [placeholder(dim) for dim in args]
+
+
+def mlp(x, hidden_sizes=(32,), activation=tf.tanh, output_activation=None):
+    for h in hidden_sizes[:-1]:
+        x = tf.layers.dense(x, units=h, activation=activation)
+    return tf.layers.dense(x, units=hidden_sizes[-1], activation=output_activation)
+
+
+def placeholder_from_space(space):
+    if isinstance(space, Box):
+        return placeholder(space.shape[0])
+    elif isinstance(space, Discrete):
+        return tf.placeholder(dtype=tf.int32, shape=(None,))
+    raise NotImplementedError
+
+
+def placeholders_from_spaces(*args):
+    return [placeholder_from_space(space) for space in args]
+
+
+def get_vars(scope):
+    return [x for x in tf.global_variables() if scope in x.name]
+
+
+def count_vars(scope):
+    v = get_vars(scope)
+    return sum([np.prod(var.shape.as_list()) for var in v])
+
+
+"""
+Actor-Critics
+"""
+def mlp_actor_critic(x, a, hidden_sizes=(400, 300), activation=tf.nn.relu,
+                     output_activation=tf.tanh, action_space=None):
+    act_dim = action_space.n
+    with tf.variable_scope('q'):
+        # Q value for each action
+        q = mlp(x, list(hidden_sizes)+[act_dim], activation, output_activation)
+    return q
