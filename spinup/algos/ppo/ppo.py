eaef54e81381f6cb805466935b8b469672d94d01
George Matheos <georgematheos@gmail.com>
2019-02-23 16:26:45 -0800

    filled in and documented outline of ppo implementation

--- spinup/algos/ppo/ppo.py
+++ spinup/algos/ppo/ppo.py
@@ -1,8 +1,12 @@
 import numpy as np
 import tensorflow as tf
+import gym
+import time
 import core
 
-from spiningup.utils.mpi_tools import mpi_sctatistics_scalar
+from spinup.utils.mpi_tf import MpiAdamOptimizer, sync_all_params
+from spinup.utils.mpi_tools import mpi_sctatistics_scalar, mpi_fork, mpi_avg, proc_id, num_procs
+from spinup.utils.logx import EpochLogger
 
 class PPOBuffer:
   """
@@ -175,3 +179,228 @@ class PPOBuffer:
     self.adv_buf = (self.adv_buf - adv_mean) / adv_std
 
     return [self.obs_buf, self.act_buf, self.adv_buf, self.ret_buf, self.logp_buf]
+
+"""
+
+"Proximal Policy Optimization (by clipping), 
+
+with early stopping based on approximate KL" -OpenAI
+
+"""
+def ppo(env_fn,
+  # by default, use the neural network mlp we define in core
+  actor_critic=core.mlp_actor_critic,
+  ac_kwargs=dict(),
+  seed=0,
+  steps_per_epoch=4000,
+  epochs=50,
+  gamma=0.99,
+  clip_ratio=0.2,
+  pi_lr=3e-4,
+  vf_lr=1e-3,
+  train_pi_iters=80,
+  train_v_iters=80,
+  lam=0.97,
+  max_ep_len=1000,
+  target_kl=0.01,
+  logger_kwargs=dict(),
+  save_freq=10):
+  """
+  "Args:
+  env_fn: A function which creates a copy of the environment.
+  The environment must satisfy the OpenAI Gym API.
+
+  actor_critic: A function with takes in placeholder symbols
+  for state, ``x_ph``, and action ``a_ph``, and returns the main
+  outputs from the agent's Tensorflow computation graph:
+
+  ===========  ================  ======================================
+  Symbol       Shape             Description
+  ===========  ================  ======================================
+  ``pi``       (batch, act_dim)  | Samples actions from policy given states.
+  ``logp``     (batch,)          | Gives log probability according to
+                                  | the policy, of taking actions ``a_ph``
+                                  | in states ``x_ph``.
+  ``logp_pi``  (batch,)          | Gives log probability, according to
+                                  | the policy, of the action sampled by ``pi``.
+  ``v``        (batch,)          | Gives the value estimate for states
+                                  | in ``x_ph``.  (Critical: make sure
+                                  | to flatten this!)
+  ===========  ================  ======================================" -OpenAI
+  Okay, quick interruption to OpenAI documentation here.
+  actor_critic is the function which interfaces with tensorflow.  It takes in
+  ``x_ph`` (x placeholder), ie. a representation of the current state, and
+  ``a_ph``, a representation of the some actions.  (TODO: document
+  *what* these actions are).
+  actor_critic runs these inputs through the tensorflow graph and returns several
+  pieces of information that are relevant to PPO; these are described above.
+
+  Back to OpenAI:
+  "
+  ac_kwargs (dict): Any kwargs appropriate for actor_critic function
+      you provided to PPO.
+
+  seed (int): Seed for random number generators.
+
+  setps_per_epoch (int): Number of steps of interaction (state-action pairs)
+      for the agent and the environment in each epoch.
+
+  epochs (int): Number of epochs of interaction (equivalent to
+      number of policy updates) to perform.
+
+  gamma (float): Discount factor. (Always between 0 and 1.)
+
+  clip_ratio (float): Hyperparameter for clipping in the policy objective.
+      Roughly: how far can the new policy go from the old policy while
+      still profiting (improving the objective function)? The new policy
+      can still go farther than the clip_ratio says, but it doesn't help
+      on the objective anymore.  (Usually small, 0.1 to 0.3.)
+
+  pi_lr (float): Learning rate for policy optimizer.
+
+  vf_lr (float): Learning rate for value function optimizer.
+
+  train_pi_iters (int): Maximum number of gradient descent steps to take
+      on policy loss per epoch.  (Early stopping may cause optimizer
+      to take fewer than this.)
+
+  train_v_iters (int): Number of gradient descent steps to take on
+      value funciton per epoch.
+
+  lam (float): Lambda for GAE-Lambda. (Always between 0 and 1,
+      close to 1).
+
+  max_ep_len (int): Maximum length of trajectory / episode / rollout.
+
+  target_kl (float): Roughly what KL divergence we think is appropriate
+      between new and old policies after an update.  This will get used
+      for early stopping.  (Usually small, 0.01 or 0.05.)
+
+  logger_kwargs (dict): Keyword args for EpochLogger.
+
+  save_freq (int): How often (in terms of gap between epochs) to save
+      the current policy and value function." - OpenAI
+  """
+  logger = EpochLogger(**logger_kwargs)
+  logger.save_config(locals())
+
+  # modify the seed based on the process so if
+  # we run this in multiple processes
+  # simultaneously we don't do the
+  # exact same thing
+  seed += 10000 * proc_id()
+  # set up our random stuff with this seed
+  tf.set_random_seed(seed)
+  np.random.seed(seed)
+
+  # create the environment
+  env = env_fn()
+  obs_dim = env.observation_space.shape
+  act_dim = env.action_space.shape
+
+  # tell the policy (implemented in actor_critic function) what the action space is
+  ac_kwargs['action_space'] = env.action_space
+
+  # "Inputs to computation graph" -OpenAI
+  # create tensorflow placeholders for observations (x_ph), actions (a_ph),
+  # advantages (adv_ph), returns (ret_ph), log probabilities from the prev run (logp_old_ph)
+  x_ph, a_ph = core.placeholders_from_spaces(env.observation_space, env.action_space)
+  adv_ph, ret_ph, logp_old_ph = core.placeholders(None, None, None)
+
+  # "Main outputs from computation graph" -OpenAI
+  # essentially here we fill in the tensorflow graph so we can compute
+  # the pi, logp, logp_pi, and v tensors based on the
+  # x_ph and a_ph we created above
+  pi, logp, logp_pi, v = actor_critic(x_ph, a_ph, **ac_kwargs)
+
+  # "Need all placeholders in *this* order later (to zip with data from buffer)" -OpenAI
+  all_phs = [x_ph, a_ph, adv_ph, ret_ph, logp_old_ph]
+
+  # "Every step, get: action, value, and logprob" -OpenAI
+  # we later feed this list into tf.session.run()
+  # to tell it to compute the value of pi, v, logp_pi
+  # using the tensorflow graph we have created
+  get_action_ops = [pi, v, logp_pi]
+
+  # Experience buffer
+
+  # number of steps per epoch per process
+  local_steps_per_epoch = int(steps_per_epoch / num_procs())
+
+  buf = PPOBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
+
+  # Count the number of parameters we are gonna be training,
+  # both for the policy and for the value function
+  var_counts = tuple(core.count_vars(scope) for scope in ['pi', 'v'])
+  logger.log('\nNumber of parameters: \t pi: %d, \t v: %d\n'%var_counts)
+
+  # PPO objectives
+  # TODO
+
+  # Info (useful to watch during learning)
+  # TODO
+
+  #Optimizers
+  # TODO
+
+  # initialize the tensorflow computation graph's parameters
+  # with values
+  sess = tf.Session()
+  sess.run(tf.global_variables_initializer())
+
+  # "Sync params across processes" -OpenAI
+  sess.run(sync_all_params())
+
+  # Setup model saving
+  logger.setup_tf_saver(sess, inputs={'x': x_ph}, outputs={'pi': pi, 'v': v})
+
+  def update():
+    # TODO
+    pass
+
+  start_time = time.time()
+
+  # initialize the variables we use while training
+  # o = observation (env.reset() returns initial observation)
+  # r = reward = (starts as 0)
+  # d = done? (whether current episode in env is over)
+  # ep_ret = episode return
+  # ep_len = length of episode so far
+  o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+
+  # "Main loop: collect experience in env and update/log each epoch"
+  for epoch in range(epochs):
+    for t in range(local_steps_per_epoch):
+      
+      # run the computation of the action, value function, and probability of the action
+      # using the most recent observation in the x_ph slot
+      a, v_t, logp_t = sess.run(get_action_ops, feed_dict={x_ph: o.reshape(1,-1)})
+
+      # save and log
+      buf.store(o, a, r, v_t, logp_t)
+      logger.store(VVals=v_t)
+
+      # take the action we computed and advance the environment
+      o, r, d, _ = env.step(a[0])
+      ep_ret += r
+      ep_len += 1
+
+      terminal = d or (ep_len == max_ep_len)
+      if terminal or (t==local_steps_per_epoch - 1):
+        # TODO
+        pass
+
+    # every save_freq epochs,
+    # save the state of the environment
+    # also save the current state of our value function model
+    # and policy
+    # these are automatically saved by the save_state function
+    # since we have already called logger.setup_tf_saver
+    if (epoch % save_freq == 0) or (epoch == epochs - 1):
+      logger.save_state({'env': env}, None)
+
+    # perform PPO update!
+    update()
+    
+    # Log info about epoch
+    # TODO
