d55b69e281a3f055c4685f51451f3a61ecb0ec38
George Matheos <georgematheos@gmail.com>
2019-03-02 13:39:10 -0800

    added main running functionality copied from openai original implementation

--- spinup/algos/ppo/ppo.py
+++ spinup/algos/ppo/ppo.py
@@ -510,3 +510,29 @@ def ppo(env_fn,
     logger.log_tabular('StopIter', average_only=True)
     logger.log_tabular('Time', time.time() - start_time)
     logger.dump_tabular()
+
+# Just copied this in from the original OpenAI implementation
+# for testing
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+    parser.add_argument('--hid', type=int, default=64)
+    parser.add_argument('--l', type=int, default=2)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--seed', '-s', type=int, default=0)
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--steps', type=int, default=4000)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--exp_name', type=str, default='ppo')
+    args = parser.parse_args()
+
+    mpi_fork(args.cpu)  # run parallel code with mpi
+
+    from spinup.utils.run_utils import setup_logger_kwargs
+    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
+
+    ppo(lambda : gym.make(args.env), actor_critic=core.mlp_actor_critic,
+        ac_kwargs=dict(hidden_sizes=[args.hid]*args.l), gamma=args.gamma, 
+        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
+        logger_kwargs=logger_kwargs)
