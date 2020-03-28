e026c4ae38f72e4f0a8275ea48f1bee17cd28e06
haotian <ritou11@gmail.com>
2020-03-28 09:21:37 +0800

    test_policy: deterministic option for pytorch

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -101,7 +101,7 @@ def load_pytorch_policy(fpath, itr, deterministic=False):
     def get_action(x):
         with torch.no_grad():
             x = torch.as_tensor(x, dtype=torch.float32)
-            action = model.act(x)
+            action = model.act(x) if not deterministic else model.act(x, True)
         return action
 
     return get_action
