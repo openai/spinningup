16965f5766c371dc7286c9cc4dc2e0788a79dd8a
Alon Burg <burgalon@gmail.com>
2020-10-12 15:55:02 +0300

    tqdm auto

--- spinup/algos/pytorch/ppo_shared/ppo_shared.py
+++ spinup/algos/pytorch/ppo_shared/ppo_shared.py
@@ -8,7 +8,7 @@ from spinup.algos.pytorch.ppo_shared.impala_cnn import ImpalaCNNActorCritic
 from spinup.utils.logx import EpochLogger
 from spinup.utils.mpi_pytorch import setup_pytorch_for_mpi, sync_params, mpi_avg_grads
 from spinup.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 class PPOBuffer:
     """
