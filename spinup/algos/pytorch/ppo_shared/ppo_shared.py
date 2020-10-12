e404b39ea0acc7a7ad63057dd63e344996acb54a
Alon Burg <burgalon@gmail.com>
2020-10-12 17:46:46 +0300

    gpu fix

--- spinup/algos/pytorch/ppo_shared/ppo_shared.py
+++ spinup/algos/pytorch/ppo_shared/ppo_shared.py
@@ -322,7 +322,7 @@ def ppo(env_fn, actor_critic=ImpalaCNNActorCritic, ac_kwargs=dict(), seed=0,
                     print('Warning: trajectory cut off by epoch at %d steps.'%ep_len, flush=True)
                 # if trajectory didn't reach terminal state, bootstrap value target
                 if timeout or epoch_ended:
-                    _, v, _ = ac.step(torch.as_tensor(o, dtype=torch.float32, device=device))
+                    _, v, _ = ac.step(torch.as_tensor(o[None], dtype=torch.float32, device=device))
                 else:
                     v = 0
                 buf.finish_path(v)
