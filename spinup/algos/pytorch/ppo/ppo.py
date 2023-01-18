92e835d36d0502a5a8121782ca64149ede0e7bd2
ThucHK <hoangkimthuc.ryan@gmail.com>
2023-01-18 02:45:03 +0000

    disable mpi for logic debugging

--- spinup/algos/pytorch/ppo/ppo.py
+++ spinup/algos/pytorch/ppo/ppo.py
@@ -367,7 +367,7 @@ if __name__ == '__main__':
     parser.add_argument('--exp_name', type=str, default='ppo')
     args = parser.parse_args()
 
-    mpi_fork(args.cpu)  # run parallel code with mpi
+    # mpi_fork(args.cpu)  # run parallel code with mpi
 
     from spinup.utils.run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
