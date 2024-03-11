37fb50b2533971751757659b0ab8a14fb6aa3632
CodyClop <51699333+CodyClop@users.noreply.github.com>
2024-03-11 07:01:05 +0100

    Update ppo.py

--- spinup/algos/pytorch/ppo/ppo.py
+++ spinup/algos/pytorch/ppo/ppo.py
@@ -205,12 +205,12 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
     np.random.seed(seed)
 
     # Instantiate environment
-    env = env_fn()
+    env = env_fn
     obs_dim = env.observation_space.shape
     act_dim = env.action_space.shape
 
     # Create actor-critic module
-    ac = actor_critic(env.observation_space, env.action_space, **ac_kwargs)
+    ac = actor_critic(**ac_kwargs)
 
     # Sync params across processes
     sync_params(ac)
@@ -292,14 +292,14 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
     # Prepare for interaction with environment
     start_time = time.time()
-    o, ep_ret, ep_len = env.reset(), 0, 0
+    o, _, ep_ret, ep_len = env.reset(), 0, 0
 
     # Main loop: collect experience in env and update/log each epoch
     for epoch in range(epochs):
         for t in range(local_steps_per_epoch):
             a, v, logp = ac.step(torch.as_tensor(o, dtype=torch.float32))
 
-            next_o, r, d, _ = env.step(a)
+            next_o, r, d, _, _ = env.step(a)
             ep_ret += r
             ep_len += 1
 
@@ -326,7 +326,7 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
                 if terminal:
                     # only save EpRet / EpLen if trajectory finished
                     logger.store(EpRet=ep_ret, EpLen=ep_len)
-                o, ep_ret, ep_len = env.reset(), 0, 0
+                o, _, ep_ret, ep_len = env.reset(), 0, 0
 
 
         # Save model
