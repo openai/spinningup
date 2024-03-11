ba5b1b9f2c2483a2656a715d1d2b9aa13bcb81de
CodyClop <51699333+CodyClop@users.noreply.github.com>
2024-03-11 06:55:40 +0100

    Update __init__.py

--- spinup/__init__.py
+++ spinup/__init__.py
@@ -4,12 +4,12 @@ import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # Algorithms
-from spinup.algos.tf1.ddpg.ddpg import ddpg as ddpg_tf1
-from spinup.algos.tf1.ppo.ppo import ppo as ppo_tf1
-from spinup.algos.tf1.sac.sac import sac as sac_tf1
-from spinup.algos.tf1.td3.td3 import td3 as td3_tf1
-from spinup.algos.tf1.trpo.trpo import trpo as trpo_tf1
-from spinup.algos.tf1.vpg.vpg import vpg as vpg_tf1
+# from spinup.algos.tf1.ddpg.ddpg import ddpg as ddpg_tf1
+# from spinup.algos.tf1.ppo.ppo import ppo as ppo_tf1
+# from spinup.algos.tf1.sac.sac import sac as sac_tf1
+# from spinup.algos.tf1.td3.td3 import td3 as td3_tf1
+# from spinup.algos.tf1.trpo.trpo import trpo as trpo_tf1
+# from spinup.algos.tf1.vpg.vpg import vpg as vpg_tf1
 
 from spinup.algos.pytorch.ddpg.ddpg import ddpg as ddpg_pytorch
 from spinup.algos.pytorch.ppo.ppo import ppo as ppo_pytorch
